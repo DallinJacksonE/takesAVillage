903c2cdded4135c67d7cd96611f0ee287cdab67f
dallin <dallin@djackson.dev>
2026-05-12 15:27:45 -0600

    name fix

--- service/constants/default.py
+++ service/constants/default.py
@@ -30,10 +30,10 @@ PHASE_LENGTH = 120
 
 GAME_LENGTH = 15
 
-NAMES_LIST = ["Bork", "Torq", "Loki", "Snort", "Smoky", "Larry", "Ig",
-              "Irates", "Kranak", "Areril",
-              "Keenmaw", "Lerk", "Brarx", "Krateges", "Krazz", "Gliregg",
-              "Tresagg", "Meemigg", "Nemarx", "Faril", "Stusz"]
+AVAILABLE_NAMES = ["Bork", "Torq", "Loki", "Snort", "Smoky", "Larry", "Ig",
+                   "Irates", "Kranak", "Areril",
+                   "Keenmaw", "Lerk", "Brarx", "Krateges", "Krazz", "Gliregg",
+                   "Tresagg", "Meemigg", "Nemarx", "Faril", "Stusz"]
 DEFAULT_SICKNESS = .03
 
 HUNGER_SICKNESS_INCREASE = .2
