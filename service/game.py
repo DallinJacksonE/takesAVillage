f131a8af2f7c22c6cd55785f3b86ea1951085496
dallin <dallin@djackson.dev>
2026-05-12 14:31:08 -0600

    contract factory update

--- service/game.py
+++ service/game.py
@@ -42,7 +42,8 @@ class Game:
         self.players = {}
         self.developments = {}
         self.map_data = {}
-        self.contract_factory = ContractFactory(self.players)
+        self.contract_factory = ContractFactory(
+            self.players, self.developments)
         self.chat_messages = []
         self.player_history = {}
         self.map_history = {}
@@ -98,7 +99,6 @@ class Game:
             self.resolve_night_phase()
             self.day += 1
             self.start_phase('WORK')
-            
 
     def start_phase(self, phase_name):
         self.phase = phase_name
