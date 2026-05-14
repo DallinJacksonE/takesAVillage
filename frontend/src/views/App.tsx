4ae65971a909d9fbcbe56c35b42a1af02f4bd6ff
dallin <dallin@djackson.dev>
2026-05-14 12:48:23 -0600

    grid rather than flex grid

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -4,40 +4,40 @@ import Home from "./Home";
 import Play from "./Play";
 import Instructions from "./Instructions";
 import Research from "./Research";
-import Gameplay from "./Gameplay"; // Extension is resolved automatically
+import Gameplay from "./Gameplay";
 import "./App.css";
 
 const App: React.FC = () => {
-	return (
-		<Router>
-			<div className='container'>
-				<header className='nav-header'>
-					<Link to='/' style={{ textDecoration: "none", color: "inherit" }}>
-						<h2>Takes a Village</h2>
-					</Link>
-					<nav>
-						<Link to='/play' className='nav-link'>
-							Play
-						</Link>
-						<Link to='/instructions' className='nav-link'>
-							Instructions
-						</Link>
-						<Link to='/research' className='nav-link'>
-							Research
-						</Link>
-					</nav>
-				</header>
+  return (
+    <Router>
+      <div className='container'>
+        <header className='nav-header'>
+          <Link to='/' style={{ textDecoration: "none", color: "inherit" }}>
+            <h2>Takes a Village</h2>
+          </Link>
+          <nav>
+            <Link to='/play' className='nav-link'>
+              Play
+            </Link>
+            <Link to='/instructions' className='nav-link'>
+              Instructions
+            </Link>
+            <Link to='/research' className='nav-link'>
+              Research
+            </Link>
+          </nav>
+        </header>
 
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/play' element={<Play />} />
-					<Route path='/game/:gameId' element={<Gameplay />} />
-					<Route path='/instructions' element={<Instructions />} />
-					<Route path='/research' element={<Research />} />
-				</Routes>
-			</div>
-		</Router>
-	);
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/play' element={<Play />} />
+          <Route path='/game/:gameId' element={<Gameplay />} />
+          <Route path='/instructions' element={<Instructions />} />
+          <Route path='/research' element={<Research />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
