3f763dfa945b1899d93615a81d85c945ecc678b7
Lane Lindstrom <lalindo.lane@gmail.com>
2026-05-15 08:14:30 -0600

    architecture

---
+++ service/constants/wealthy.py
@@ -0,0 +1,46 @@
+DEVELOPMENT_COSTS = {
+    "Farm": {
+        "build": {"wood": 2},
+        "maintain": {"wood": 2, "iron": 1},
+        "upgrade": {"wood": 5, "iron": 2}
+    },
+    "Woods": {
+        "build": {"food": 1, "wood": 1},
+        "maintain": {"food": 2, "iron": 2},
+        "upgrade": {"food": 5, "iron": 3}
+    },
+    "Mine": {
+        "build": {"wood": 2, "food": 2},
+        "maintain": {"wood": 3, "food": 3},
+        "upgrade": {"wood": 2, "food": 2, "iron": 5}
+    }
+}
+
+MAX_DEVELOPMENT_LEVEL = 5
+MAINTENANCE_DAYS = 7
+
+STARTING_INVENTORY = {
+    "wood": 5,
+    "food": 3,
+    "iron": 2,
+}
+
+CAMPFIRE_COST = {"wood": 1}
+MAX_FIRE_SEATS = 3
+
+# seconds
+PHASE_LENGTH = 120
+
+GAME_LENGTH = 15
+
+AVAILABLE_NAMES = ["Bork", "Torq", "Loki", "Snort", "Smoky", "Larry", "Ig",
+                   "Irates", "Kranak", "Areril",
+                   "Keenmaw", "Lerk", "Brarx", "Krateges", "Krazz", "Gliregg",
+                   "Tresagg", "Meemigg", "Nemarx", "Faril", "Stusz"]
+DEFAULT_SICKNESS = .03  # MUST CHANGE player.py SEPARATELY TO MATCH THIS VALUE
+
+HUNGER_SICKNESS_INCREASE = .2
+
+COLD_SICKNESS_INCREASE = .1
+
+RECOVERY_RATE = .07  # Rate for sickness chance going down
